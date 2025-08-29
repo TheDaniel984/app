245a9bc75fa5eda5fe040f4ae2d54a9fde546e1b
TheDaniel984 <theindio10@gmail.com>
2025-08-29 14:34:09 -0300

    agr vai

--- App.tsx
+++ App.tsx
@@ -1,14 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEvent } from 'expo';
+
+  interface Joke{
+    categories?: string
+    created_at?: string
+    icon_url?: string
+    id?: number
+    updated_at?: string
+    url?: string
+    value?: string
+  }
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [Joke, setJoke] = useState<Joke>({})
+
+  useEffect(() =>{
+    getJoke();
+  },[]);
+
+  async function getJoke() {
+    try{
+      const response = await axios.get<Joke>(
+        "https://api.chucknorris.io/jokes/random",
+        {
+          headers:{
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      setJoke(response.data);
+
+      
+      } catch (erro){
+        console.log(erro);
+      }
+    }
+
+
+    return(
+      <View style={styles.container}>
+      <text>a piada do dia é:{Joke.value ?? "carregando..."}</text>
+      <StatusBar style='auto'></StatusBar>
+      </View>
+    );
 }
+
 
 const styles = StyleSheet.create({
   container: {
